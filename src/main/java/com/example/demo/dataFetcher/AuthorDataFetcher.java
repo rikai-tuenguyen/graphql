876cbf6cdaa316bc27974ee0cd7b40e3ded1b3f9
Nguyen Quang Danh <danh.nguyen@rikai.technology>
2021-06-02 09:34:34 +0700

    add graphql campaign track

--- src/main/java/com/example/demo/dataFetcher/AuthorDataFetcher.java
+++ src/main/java/com/example/demo/dataFetcher/AuthorDataFetcher.java
@@ -1,17 +1,18 @@
 package com.example.demo.dataFetcher;
-import java.util.List;
+
 import com.example.demo.model.Author;
 import com.example.demo.services.AuthorService;
 import com.netflix.graphql.dgs.DgsComponent;
 import com.netflix.graphql.dgs.DgsMutation;
 import com.netflix.graphql.dgs.DgsQuery;
 import com.netflix.graphql.dgs.InputArgument;
+import java.util.List;
 
 @DgsComponent
 public class AuthorDataFetcher {
 
 	private final AuthorService authorService;
-	
+
   public AuthorDataFetcher(AuthorService authorService) {
     this.authorService = authorService;
 }
@@ -20,17 +21,17 @@ public class AuthorDataFetcher {
   public List<Author> findAllAuthors() {
     return authorService.findAllAuthors();
   }
-  
+
   @DgsQuery
   public List<Author> top100Author() {
     return authorService.top100Author();
   }
-  
+
   @DgsMutation
   public Author createAuthor(@InputArgument String name, Integer age) {
   	Author author = new Author(name, age);
   	Author newAuthor = authorService.createAuthor(author);
- 
+
 		return newAuthor;
   }
 }
